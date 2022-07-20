0dca6ceadb575f2849317b8f74a32c07f2762837
Narendra <narendrasab1993@outlook.com>
2022-07-20 22:16:38 +0530

    pkg added: sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Content from './components/Content';
