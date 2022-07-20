c1d01786288c61b36c0f00721ccf44bc6ba7ab0b
Narendra <narendrasab1993@outlook.com>
2022-07-20 22:03:24 +0530

    added google font

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,13 +14,15 @@ function Header() {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="##">Home</a>
+                            <a className="nav-link active menu-item" aria-current="page" href="##">About us</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="##">Link</a>
+                            <a className="nav-link active menu-item" aria-current="page" href="##">Contact us</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active menu-item" aria-current="page" href="##">What we offer</a>
                         </li>
                     </ul>
-
                 </div>
             </div>
         </nav>
