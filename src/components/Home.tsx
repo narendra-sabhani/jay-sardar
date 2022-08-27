fdc68fbc8c7e5ccf45f8483ac3904deb2e40344d
Narendra <narendrasab1993@outlook.com>
2022-08-27 10:16:34 +0530

    home page design started

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,35 @@
 export function Home() {
     return (
         <>
-            {/* <div className="row p-2">
+            <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="false">
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src={process.env.PUBLIC_URL + "img/slider-1.jpg"} className="d-block w-100" height={350} alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src={process.env.PUBLIC_URL + "img/slider-2.jpg"} className="d-block w-100" height={350} alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src={process.env.PUBLIC_URL + "img/slider-3.jpg"} className="d-block w-100" height={350} alt="..." />
+                    </div>
+
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+
+            <div className="row pt-4">
                 <div className="col-sm-6">
                     <div className="card">
                         <div className="card-body">
@@ -21,8 +49,28 @@ export function Home() {
                         </div>
                     </div>
                 </div>
-            </div> */}
-            <h1>Home</h1>
+            </div>
+
+            <div className="row pt-4">
+                <div className="col-sm-6">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Special title treatment</h5>
+                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="##" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-6">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Special title treatment</h5>
+                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="##" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
